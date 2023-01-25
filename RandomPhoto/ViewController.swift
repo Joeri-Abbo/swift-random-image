dbde5a525a3dbfc8b13f06e6ffa9b0858ae3d9b2
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-25 11:57:07 +0100

    Add image and random color on click of button

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -9,11 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+    
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+    
+        return imageView
+    }()
+    
+    
+    private let button: UIButton = {
+        let button = UIButton()
+    
+
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+    
+        return button
+    }()
+    
+    let colors = [
+        UIColor.systemPink,
+        UIColor.systemBlue,
+        UIColor.systemGreen,
+        UIColor.systemYellow,
+        UIColor.systemPurple,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        
+        imageView.frame = CGRect(
+            x: 0,
+            y:0,
+            width: 300,
+            height: 300
+        )
+        imageView.center = view.center
+        
+        view.addSubview(button)
+    
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        getRandomPhoto()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(
+            x: 20,
+            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 55
+        )
+        
+    }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 }
 
